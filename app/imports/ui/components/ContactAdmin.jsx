dc30af8d4b2dade3bb4f02e8bb2f6df5b067c343
Chak Hon Lam <chakhon@hawaii.edu>
2020-11-06 21:51:02 -1000

    contact forms finished in 22 minutes

--- app/imports/ui/components/ContactAdmin.jsx
+++ app/imports/ui/components/ContactAdmin.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Card, Image } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
-import { withRouter } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 class ContactAdmin extends React.Component {
@@ -22,6 +22,9 @@ class ContactAdmin extends React.Component {
           </Card.Content>
           <Card.Content extra>
             {this.props.contact.owner}
+          </Card.Content>
+          <Card.Content extra>
+            <Link to={`/edit/${this.props.contact._id}`}>Edit</Link>
           </Card.Content>
         </Card>
     );
